4d3553fb649b287a9356e576378de41334ccf22b
rodrigofnobrega <rofen1@protonmail.com>
2023-05-24 19:55:50 -0300

    Removal of unused code and test code

--- src/main.py
+++ src/main.py
@@ -1,48 +1,15 @@
 from pytube import YouTube
-from multiprocessing import Process
-from moviepy.editor import *
-from moviepy.video.io.ffmpeg_tools import ffmpeg_merge_video_audio
-import asyncio
-from utils.info_video import InfoVideo
-from download import Download
-
-
-async def Convert(yt: YouTube):
-    print("Convertendo")
-    download_video = asyncio.create_task(DownloadVideo(yt))
-    download_audio = asyncio.create_task(DownloadAudio(yt))
-
-    await download_video, download_audio
-
-    output_audio = "audio.mp3"
-
-    videoclip = VideoFileClip("video.mp4")
-    audioclip = AudioFileClip("audio.mp4")
-    file = audioclip.write_audiofile(output_audio)
-
-    print(file)
-
-    ffmpeg_merge_video_audio(video=videoclip.filename, audio=output_audio, output="audiovideo.mp4")
-    '''new_audioclip = CompositeAudioClip([audioclip])
-    videoclip.audio = new_audioclip
-    videoclip.write_videofile("audiovideo.mp4", threads=4)'''
-
-async def DownloadVideo(yt: YouTube):
-    print("Downloading Video")
-    yt.streams.filter(adaptive=True, file_extension="mp4").first().download(filename="video.mp4")
-
-async def DownloadAudio(yt: YouTube):
-    print("Downloading Audio")
-    yt.streams.filter(adaptive=True, file_extension="mp4", only_audio=True).first().download(filename="audio.mp4")
+from src.modules.utils.info_video import InfoVideo
+from src.modules.download import Download
 
 def main() -> None:
     print('''=======YOUTUBE DOWNLOADER=======\n''')
     
-    video_link: str = "https://www.youtube.com/watch?v=K-a8s8OLBSE"
+    video_link: str = str(input("Video link: "))
 
     yt: YouTube = YouTube(video_link)
 
-    output_path: str = "/home/rods/code/python/youtube_downloader/" 
+    output_path: str =  str(input("Download directory: "))
 
     info_video: InfoVideo = InfoVideo()
     info_video.set_filename_sufix(".mp3")
