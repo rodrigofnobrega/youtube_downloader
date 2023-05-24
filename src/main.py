8688b4617689714ce402a37256946297fcce36a4
rodrigofnobrega <rofen1@protonmail.com>
2023-05-24 20:18:39 -0300

    Fix import error (InfoVideo)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
-from src.modules.utils.info_video import InfoVideo
+from src.utils.info_video import InfoVideo
 from src.modules.download import Download
 
 def main() -> None:
@@ -9,7 +9,7 @@ def main() -> None:
 
     yt: YouTube = YouTube(video_link)
 
-    output_path: str =  str(input("Download directory: "))
+    '''output_path: str =  str(input("Download directory: "))
 
     info_video: InfoVideo = InfoVideo()
     info_video.set_filename_sufix(".mp3")
@@ -22,7 +22,7 @@ def main() -> None:
 
     downloader: Download = Download(yt=yt, only_audio=info_video.get_only_audio(), only_video=info_video.get_only_video(), adaptive=info_video.get_adaptive(), output_path=info_video.get_output_path(), filename=info_video.get_filename())
 
-    downloader.Downloader()
+    downloader.downloader()'''
     
 
 if __name__ == "__main__":
