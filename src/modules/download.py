8b6dae8a3a8b663b664bf38eee22b82b80f5b1ad
rodrigofnobrega <rofen1@protonmail.com>
2023-05-25 13:29:37 -0300

    Created private variables, removed unnecessary variables, change paramater func downloader

--- src/modules/download.py
+++ src/modules/download.py
@@ -3,24 +3,18 @@ from typing import Any
 from pytube import YouTube, Stream
 
 class Download:
-    yt: YouTube = None
-    only_audio: bool = None
-    only_video: bool = None
-    adaptive: bool = None
-    output_path: str = None
-    filename: str = None
-    stream: Stream = None
+    __yt: YouTube = None
+    __output_path: str = None
+    __filename: str = None
+    __stream: Stream = None
 
-    def __init__(self, yt: YouTube, only_audio: bool, only_video: bool, adaptive: bool, output_path: str, filename: str) -> None:
-        self.yt = yt
-        self.only_audio = only_audio
-        self.only_video = only_video
-        self.adaptive = adaptive
-        self.output_path = output_path
-        self.filename = filename
+    def __init__(self, yt: YouTube, output_path: str, filename: str) -> None:
+        self.__yt = yt
+        self.__output_path = output_path
+        self.__filename = filename
 
     def bar_download_progress(self, chunk, file_handle, bytes_remaining: bytes):
-        filesize = self.stream.filesize
+        filesize = self.__stream.filesize
         current = ((filesize - bytes_remaining) / filesize)
         percent = ('{0:.1f}').format(current * 100)
         progress = int(50 * current)
@@ -34,13 +28,13 @@ class Download:
         Output path => {output_path}
         File name => {filename}
         File size => {filesize}Mb
-        '''.format(output_path=self.output_path, filename=self.filename, filesize=self.stream.filesize_mb))
+        '''.format(output_path=self.__output_path, filename=self.__filename, filesize=self.__stream.filesize_mb))
         
-    def downloader(self):
-        self.stream = self.yt.streams.filter(file_extension="mp4", only_audio=self.only_audio, only_video=self.only_video, adaptive=self.adaptive).first()
-
-        self.yt.register_on_progress_callback(self.bar_download_progress)
-        self.yt.register_on_complete_callback(self.info_about_download)
+    def downloader(self, only_audio: bool, only_video: bool, adaptive: bool=True):
+        self.__stream = self.__yt.streams.filter(file_extension="mp4", only_audio=only_audio, only_video=only_video, adaptive=adaptive).first()
         
-        self.stream.download(output_path=self.output_path, filename=self.filename)
+        self.__yt.register_on_progress_callback(self.bar_download_progress)
+        self.__yt.register_on_complete_callback(self.info_about_download)
+        
+        self.__stream.download(output_path=self.__output_path, filename=self.__filename)
         
