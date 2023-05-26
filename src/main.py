dd65856efe726c3c3def8585ecd8fe7c8f72245d
rodrigofnobrega <rofen1@protonmail.com>
2023-05-26 19:48:52 -0300

    Revert "Renamed variables"
    This reverts commit 56c29e049ac71335cd9739131f8c7b3653b53017.

--- src/main.py
+++ src/main.py
@@ -7,25 +7,27 @@ def main() -> None:
     print('''=======YOUTUBE DOWNLOADER=======\n''')
     
     video_link: str = str(input("Video link: "))
-    download_path: str =  str(input("Download directory: "))
-    
+
     yt: YouTube = YouTube(video_link)
 
+    filepath: str =  str(input("Download directory: "))
+
     info_video: InfoVideo = InfoVideo()
+    info_video.set_filename_sufix(".mp3")
     info_video.set_video_title(yt.title)
-    info_video.set_download_path(download_path)
+    info_video.set_output_path(filepath)
     info_video.set_only_audio(True)
     info_video.set_only_video(False)
     info_video.set_adaptive(True)
-    info_video.set_file_path()
+    info_video.set_filename()
 
-    downloader: Download = Download(yt=yt, output_path=download_path, filename=info_video.get_file_path())
+    downloader: Download = Download(yt=yt, output_path=filepath, filename=info_video.get_filename())
 
     downloader.downloader(only_audio=info_video.get_only_audio(), only_video=info_video.get_only_video(), adaptive=info_video.get_adaptive())
 
     convert: Convert = Convert()
 
-    convert.convert_audio(filepath=info_video.get_file_path(), filename=info_video.get_filename())
+    convert.convert_audio(filepath=filepath, filename=info_video.get_filename())
 
 if __name__ == "__main__":
     main()
