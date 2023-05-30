54bb57470d91efdc02cb34b5531a0012d12cda45
rodrigofnobrega <rofen1@protonmail.com>
2023-05-30 20:35:56 -0300

    Added a loop to download an unspecified number of videos

--- src/main.py
+++ src/main.py
@@ -14,31 +14,34 @@ def main() -> None:
     type_download: int = int(input('''[0] => Download Audio
 [1] => Download Video
 Type of download: '''))
+    
+    while True:
+        yt: YouTube = YouTube(video_link)
 
-    yt: YouTube = YouTube(video_link)
+        info_video: InfoVideo = InfoVideo()
+        info_video.set_video_title(yt.title)
+        info_video.set_download_dir(download_dir)
+        info_video.set_file_path(download_dir, yt.title)
 
-    info_video: InfoVideo = InfoVideo()
-    info_video.set_video_title(yt.title)
-    info_video.set_download_dir(download_dir)
-    info_video.set_file_path(download_dir, yt.title)
+        downloader: Download = Download(yt=yt, output_path=download_dir, filename=yt.title)
+        convert: Convert = Convert()
 
-    downloader: Download = Download(yt=yt, output_path=download_dir, filename=yt.title)
-    convert: Convert = Convert()
+        if type_download == 0:
+            downloader.downloader(only_audio=True)
+            convert.convert_to_mp3(filepath=info_video.get_file_path())
 
-    if type_download == 0:
-        downloader.downloader(only_audio=True)
-        convert.convert_to_mp3(filepath=info_video.get_file_path())
+        else:
+            downloader.downloader(only_audio=True)
+            convert.convert_to_mp3(filepath=info_video.get_file_path())
+            
+            downloader.downloader(only_video=True)
+            convert.merge_video_audio(video_file=info_video.get_file_path(), audio_file_path=info_video.get_file_path()+".mp3", output_file=info_video.get_file_path()+".mp4")
+            
+            remove(info_video.get_file_path()+".mp3")
 
-    else:
-        downloader.downloader(only_audio=True)
-        convert.convert_audio(filepath=info_video.get_file_path())
-        
-        downloader.downloader(only_video=True)
-        convert.merge_video_audio(video_file=info_video.get_file_path(), audio_file_path=info_video.get_file_path()+".mp3", output_file=info_video.get_file_path()+".mp4")
-        
-        remove(info_video.get_file_path()+".mp3")
+        remove(info_video.get_file_path())
 
-    remove(info_video.get_file_path())
+        video_link = str(input("Video link: "))
 
 if __name__ == "__main__":
     main()
